88b858fb9e40a4a1062d28790fafe5eb526305cf
Raki3154 <152172785+Raki3154@users.noreply.github.com>
2025-06-08 17:35:40 +0530

    Update ahome.php

--- ahome.php
+++ ahome.php
@@ -19,29 +19,28 @@
 	</head>
 	<body>
 		<div id="container">	
-		<div id="header">
-			<h1>Read Verse</h1>
-		</div>
-		<div id="wrapper">
-			<h3 id="header">Welcome Admin</h3>
-			<div id="center">
-				<ul class="record">
-					<li> Total Students : <?php echo countRecord("select * from student", $conn);?></li>
-					<li> Total Books : <?php echo countRecord("select * from book", $conn);?></li>
-					<li> Total Request : <?php echo countRecord("select * from request", $conn);?></li>
-					<li> Total Comments : <?php echo countRecord("select * from comment", $conn);?></li>
-				</ul>
+			<div id="header">
+				<h1>Read Verse</h1>
 			</div>
-		</div> 
-		<div id="navi">
-			<?php
-				include "adminside.php"
-			?>
-		</div> 
-		<div id="footer">
-			<p>Copyright &copy;Rakkesh</p>
-		</div>
-
+			<div id="wrapper">
+				<h3 id="header">Welcome Admin</h3>
+				<div id="center">
+					<ul class="record">
+						<li> Total Students : <?php echo countRecord("select * from student", $conn);?></li>
+						<li> Total Books : <?php echo countRecord("select * from book", $conn);?></li>
+						<li> Total Request : <?php echo countRecord("select * from request", $conn);?></li>
+						<li> Total Comments : <?php echo countRecord("select * from comment", $conn);?></li>
+					</ul>
+				</div>
+			</div> 
+			<div id="navi">
+				<?php
+					include "adminside.php"
+				?>
+			</div> 
+			<div id="footer">
+				<p>Copyright &copy;Rakkesh</p>
+			</div>
 		</div>
 	</body>
 </html>
